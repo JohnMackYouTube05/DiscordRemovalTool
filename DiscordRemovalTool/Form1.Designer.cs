35d1cf2d41000cf387f9ff26cf56e9ca8d0c9de0
John Mack <liljmack05@gmail.com>
2023-01-16 12:29:00 -0600

    Add project files.

---
+++ DiscordRemovalTool/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace DiscordRemovalTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbDiscordLogo = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_IsDiscordInstalled = new System.Windows.Forms.Label();
+            this.btnUninstall = new System.Windows.Forms.Button();
+            this.btnReinstall = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDiscordLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbDiscordLogo
+            // 
+            this.pbDiscordLogo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbDiscordLogo.Image = global::DiscordRemovalTool.Properties.Resources.discord_logo_white;
+            this.pbDiscordLogo.Location = new System.Drawing.Point(12, 25);
+            this.pbDiscordLogo.Name = "pbDiscordLogo";
+            this.pbDiscordLogo.Size = new System.Drawing.Size(381, 64);
+            this.pbDiscordLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbDiscordLogo.TabIndex = 0;
+            this.pbDiscordLogo.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(381, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Removal Tool v1.0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(79, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Is Discord Installed:";
+            // 
+            // lbl_IsDiscordInstalled
+            // 
+            this.lbl_IsDiscordInstalled.AutoSize = true;
+            this.lbl_IsDiscordInstalled.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold);
+            this.lbl_IsDiscordInstalled.ForeColor = System.Drawing.Color.Lime;
+            this.lbl_IsDiscordInstalled.Location = new System.Drawing.Point(245, 139);
+            this.lbl_IsDiscordInstalled.Name = "lbl_IsDiscordInstalled";
+            this.lbl_IsDiscordInstalled.Size = new System.Drawing.Size(61, 26);
+            this.lbl_IsDiscordInstalled.TabIndex = 3;
+            this.lbl_IsDiscordInstalled.Text = "YES";
+            // 
+            // btnUninstall
+            // 
+            this.btnUninstall.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btnUninstall.Location = new System.Drawing.Point(12, 184);
+            this.btnUninstall.Name = "btnUninstall";
+            this.btnUninstall.Size = new System.Drawing.Size(381, 42);
+            this.btnUninstall.TabIndex = 4;
+            this.btnUninstall.Text = "Uninstall Discord";
+            this.btnUninstall.UseVisualStyleBackColor = true;
+            this.btnUninstall.Click += new System.EventHandler(this.btnUninstall_Click);
+            // 
+            // btnReinstall
+            // 
+            this.btnReinstall.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btnReinstall.Location = new System.Drawing.Point(12, 232);
+            this.btnReinstall.Name = "btnReinstall";
+            this.btnReinstall.Size = new System.Drawing.Size(381, 42);
+            this.btnReinstall.TabIndex = 5;
+            this.btnReinstall.Text = "Reinstall Discord";
+            this.btnReinstall.UseVisualStyleBackColor = true;
+            this.btnReinstall.Click += new System.EventHandler(this.btnReinstall_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 341);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(381, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.White;
+            this.lblStatus.Location = new System.Drawing.Point(12, 322);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(38, 13);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "Ready";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(405, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnReinstall);
+            this.Controls.Add(this.btnUninstall);
+            this.Controls.Add(this.lbl_IsDiscordInstalled);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbDiscordLogo);
+            this.Name = "Form1";
+            this.Text = "Discord Removal Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbDiscordLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbDiscordLogo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_IsDiscordInstalled;
+        private System.Windows.Forms.Button btnUninstall;
+        private System.Windows.Forms.Button btnReinstall;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ DiscordRemovalTool/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Security.Principal;
+using System.Runtime.InteropServices;
+using static DiscordRemovalTool.UacHelper;
+using Microsoft.Win32;
+using System.Threading;
+
+namespace DiscordRemovalTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string DiscordDownloadPath = Environment.GetEnvironmentVariable("TEMP") + "\\DiscordSetup.exe";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bool isAdmin = UacHelper.IsAdministrator(); //Check if program is running as an Administrator.
+            if (!isAdmin)
+            {
+                MessageBox.Show("This program must be run as an administrator to work properly.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+            string DiscordPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Discord";
+            //Check if Discord is installed.
+            if (Directory.Exists(DiscordPath)) {
+                lbl_IsDiscordInstalled.Text = "YES";
+                lbl_IsDiscordInstalled.ForeColor = Color.Lime;
+            } else
+            {
+                lbl_IsDiscordInstalled.Text = "NO";
+                lbl_IsDiscordInstalled.ForeColor = Color.Red;
+                btnUninstall.Enabled = false;
+            }
+        }
+
+        private void ChangeStatus(string message)
+        {
+            lblStatus.Text = message;
+        }
+
+        private void btnUninstall_Click(object sender, EventArgs e)
+        {
+            ChangeStatus("Checking if Discord is running...");
+            Process[] DiscordProcess = Process.GetProcessesByName("Discord.exe");
+            if (DiscordProcess.Length > 0)
+            {
+                progressBar1.Value = 5;
+                ChangeStatus("Discord is running. Killing all Discord processes...");
+                foreach (Process p in DiscordProcess)
+                {
+                    ChangeStatus($"Killing Process {p.ProcessName} with ID {p.Id}...");
+                    p.Kill();
+                    if (progressBar1.Value < 15) { progressBar1.Value++; }
+                }
+            }
+            else
+            {
+                ChangeStatus("Discord is not running. Moving on...");
+                progressBar1.Value = 15;
+            }
+            ChangeStatus("Deleting Discord Start Menu shortcut...");
+            File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc\\Discord.lnk"); //Delete Discord shortcut from the Start Menu folder.
+            progressBar1.Value++;
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Discord.lnk"))
+            {
+                ChangeStatus("Deleting Discord Desktop shortcut...");
+                File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Discord.lnk");
+                progressBar1.Value = 20;
+            }
+            ChangeStatus("Deleting Discord AppData directory...");
+            string[] subdirs = Directory.GetDirectories(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\discord");
+            if (subdirs.Length > 0)
+            {
+                foreach (string dirPath in subdirs)
+                {
+                    
+                    string[] files = Directory.GetFiles(dirPath);
+                    string[] dirs = Directory.GetDirectories(dirPath);
+                    if (dirs.Length > 0)
+                    {
+                        foreach (string dir in dirs)
+                        {
+                            string[] sFiles = Directory.GetFiles(dir);
+                            foreach(string file in sFiles)
+                            {
+                                if (progressBar1.Value < 35) { progressBar1.Value++; }
+                                ChangeStatus($"Deleting {file}...");
+                                File.Delete(file);
+                            }
+                            ChangeStatus($"Deleting {dir}");
+                        }
+                    }
+                    else
+                    {
+                        ChangeStatus($"Deleting {dirPath} [Empty Directory]");
+                        Directory.Delete(dirPath, true);
+                        
+                    }
+                    if (files.Length > 0)
+                    {
+                        foreach (string file in files)
+                        {
+                            if (progressBar1.Value < 35) { progressBar1.Value++; }
+                            ChangeStatus($"Deleting {file}...");
+                            File.Delete(file);
+                        }
+                        ChangeStatus($"Deleting {dirPath}");
+                    } else
+                    {
+                        ChangeStatus($"Deleting {dirPath} [Empty Directory]");
+                        Directory.Delete(dirPath, true);
+                        progressBar1.Value = 35;
+                    }
+                }
+            }
+            string[] dirfiles = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\discord");
+            if  (dirfiles.Length > 0)
+            {
+                foreach (string file in dirfiles)
+                {
+                    ChangeStatus($"Deleting {file}...");
+                    File.Delete(file);
+                    if (progressBar1.Value < 45) { progressBar1.Value++; }
+                }
+            }
+            ChangeStatus($"Deleting {Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Discord"}");
+            Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\discord");
+            progressBar1.Value = 45;
+            ChangeStatus("Moving to Discord LocalAppData Directory.");
+            string[] LAPSubDirs = Directory.GetDirectories(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Discord");
+            if (LAPSubDirs.Length > 0)
+            {
+                foreach (string dir in LAPSubDirs)
+                {
+                    string[] sdFiles = Directory.GetFiles(dir);
+
+                    if (sdFiles.Length > 0)
+                    {
+                        foreach (string sfile in sdFiles)
+                        {
+                            ChangeStatus($"Deleting {sfile}...");
+                            File.Delete(sfile);
+                            if (progressBar1.Value < 65)
+                            {
+                                progressBar1.Value++;
+                            }
+                        }
+                        ChangeStatus($"Deleting {dir}");
+                        Directory.Delete(dir, true);
+                    }
+                    else
+                    {
+                        ChangeStatus($"Deleting {dir} [Empty Directory]");
+                        Directory.Delete(dir, true);
+                    }
+                }
+            }
+            progressBar1.Value = 65;
+            string[] DiscordLAPFiles = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Discord");
+            if (DiscordLAPFiles.Length > 0)
+            {
+                foreach (string f in DiscordLAPFiles)
+                {
+                    ChangeStatus($"Deleting {f}...");
+                    File.Delete(f);
+                    if (progressBar1.Value < 75) { progressBar1.Value++; }
+                }
+                ChangeStatus($"Deleting {Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Discord"}");
+                Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Discord");
+                progressBar1.Value = 75;
+            } else
+            {
+                ChangeStatus($"Deleting {Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Discord"} [Empty Directory]");
+                Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Discord");
+                progressBar1.Value = 75;
+            }
+
+            ChangeStatus("Deleting Discord Registry Keys...");
+            string discordKeyPath = @"Software\Discord";
+
+            using (RegistryKey discordKey =
+                Registry.CurrentUser.OpenSubKey(discordKeyPath, writable: true))
+            {
+                if (discordKey != null)
+                {
+                    ChangeStatus("Deleting \\HKEY_CURRENT_USER\\Software\\Discord");
+                    discordKey.DeleteSubKeyTree("Modules");
+                    progressBar1.Value = 100;
+                }
+            }
+
+
+            
+            string DiscordPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Discord";
+            //Check if Discord uninstalled successfully.
+            if (Directory.Exists(DiscordPath))
+            {
+                ChangeStatus("Uninstalled Discord, but some residual files/directories remain. You'll have to go through the filesystem and delete these manually.");
+                lbl_IsDiscordInstalled.Text = "YES";
+                lbl_IsDiscordInstalled.ForeColor = Color.Lime;
+            }
+            else
+            {
+                ChangeStatus("Successfully uninstalled Discord!");
+                lbl_IsDiscordInstalled.Text = "NO";
+                lbl_IsDiscordInstalled.ForeColor = Color.Red;
+                btnUninstall.Enabled = false;
+            }
+
+        }
+        private void startDownload(string downloadUrl)
+        {
+            Thread thread = new Thread(() => {
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += client_DownloadProgressChanged;
+                client.DownloadFileCompleted += client_DownloadFileCompleted;
+                
+                client.DownloadFileAsync(new Uri(downloadUrl), DiscordDownloadPath);
+            });
+            thread.Start();
+        }
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate {
+                double bytesIn = double.Parse(e.BytesReceived.ToString());
+                double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+                double percentage = bytesIn / totalBytes * 100;
+                lblStatus.Text = "Downloading DiscordSetup.exe... (" + e.BytesReceived + " of " + e.TotalBytesToReceive + " bytes)";
+                progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+            });
+        }
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate {
+                lblStatus.Text = "Completed! Running setup...";
+                using (Process DiscordSetup = new Process())
+                {
+                    DiscordSetup.StartInfo.FileName = DiscordDownloadPath;
+                    DiscordSetup.StartInfo.Verb = "runas";
+                    DiscordSetup.Start();
+                    DiscordSetup.WaitForExit();
+                    lblStatus.Text = $"Setup exited with code {DiscordSetup.ExitCode}.";
+                }
+            });
+
+        }
+        private void btnReinstall_Click(object sender, EventArgs e)
+        {
+            
+            var client = new WebClient();
+            startDownload("https://discordapp.com/api/download?platform=win");
+        }
+    }
+}
